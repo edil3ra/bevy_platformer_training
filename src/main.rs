a1308bc6c6a16d622133ff8c7741ceceae49e701
edil3ra <edil3ra@gmail.com>
2022-03-23 00:15:47 +0100

    feat: load assets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::{prelude::*, render::camera::ScalingMode, sprite::Rect, sprite::TextureAtlas};
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "kebab-case")]
+struct TextureAtlasXML {
+    width: f32,
+    height: f32,
+    #[serde(rename = "$value")]
+    subs: Vec<Subs>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Subs {
+    name: String,
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+}
+
+pub struct PlayersSheet(pub Handle<TextureAtlas>);
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 
@@ -17,6 +39,7 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
+        .add_startup_system(load_assets)
         .run();
 }
 
@@ -35,5 +58,32 @@ fn spawn_camera(mut commands: Commands) {
 
     commands.spawn_bundle(camera);
 }
-    
 
+fn load_assets(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let players_image_handle: Handle<Image> = asset_server.load("spritesheet_players.png");
+    let players_xml: TextureAtlasXML = {
+        let players_string = fs::read_to_string("assets/spritesheet_players.xml")
+            .expect("could not read spritesheet_players.png");
+        serde_xml_rs::from_str(&players_string).unwrap()
+    };
+
+    let mut players_spritesheet = TextureAtlas::new_empty(
+        players_image_handle,
+        Vec2::new(players_xml.width, players_xml.height),
+    );
+
+    for sub in players_xml.subs.iter() {
+        players_spritesheet.add_texture(Rect {
+            min: Vec2::new(sub.x, sub.y),
+            max: Vec2::new(sub.width, sub.height),
+        });
+    }
+
+    println!("{:?}", players_spritesheet);
+    let player_handle = texture_atlases.add(players_spritesheet);
+    commands.insert_resource(PlayersSheet(player_handle))
+}
